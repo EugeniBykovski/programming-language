bc26b0dca715d2744c5f238f8e26e7a4a544455b
EugeniBykovski <bykovskieugeni@mail.ru>
2019-10-08 15:25:45 +0300

    check if the expression is an application

--- index.js
+++ index.js
@@ -9,6 +9,12 @@
 */
 
 // Первая часть синтаксического анализатора
+
+/*
+  Синтаксический анализатор строит структуру данных, тип которой зависит от того, какому их этих жлементов соответствует строка.
+  Если входные данные не соответствуют ни одной из трех форм (строки, числа, слова), значит, это недопустимое выражение и анализатор выдает ошибку.
+*/
+
 function parseExpression(program) {
     program = skipSpace(program);
     let match, expr;
@@ -25,8 +31,36 @@ function parseExpression(program) {
     return parseApply(expr, program.slice(match[0].length));
 }
 
-function skipSpace(string) {
+//  Затем мы вырезаем из строки ту часть программы, которая соответствует шаблону, и передаем ее вместе с объектом выражения в функцию parseApply.
+
+function skipSpace(string) { // функция удаляет пробелы в начале каждой строки программы
     let first = string.search(/\S/);
     if (first == -1) return "";
     return string.slice(first);
+}
+
+/*
+  Если следующий символ в программе не является открывающей скобкой, значит, это не приложение,
+  и parseApply возвращает выражение, которое ему было передано.
+*/
+
+function parseApply(exep, program) {
+    program = skipSpace(program);
+    if (program[0] ~ = "(") {
+        return { expr: expr, rest: program };
+    }
+
+    program = skipSpace(program.slice(1));
+    expr = { type: "apply", operator: expr, args: [] };
+    while (program[0] != ")") {
+        let arg = parseExpression(program);
+        expr.args.push(arg.expr);
+        program = skipSpace(arg.rest);
+        if (program[0] == ",") {
+            program = skipSpace(program.slice(1));
+        } else if (program[0] != ")") {
+            throw new SyntaxError("Ожидается ',' или ')'");
+        }
+    }
+    return parseApply(expr, program.slice(1));
 }
